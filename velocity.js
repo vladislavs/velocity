cb88a9ba5e5ffd19c6d913d1ad5f7065d51c458a
Vladislavs Stashkevichs <n4rayane@gmail.com>
2016-12-15 18:03:36 +0200

    removed unnecessary

--- velocity.js
+++ velocity.js
@@ -20,12 +20,6 @@
 	if (window.jQuery) {
 		return;
 	}
-	
-	/* Array map and filter support for lt IE9 */
-	(function(fn){
-		if (!fn.map) fn.map=function(f){var r=[];for(var i=0;i<this.length;i++)r.push(f(this[i]));return r}
-		if (!fn.filter) fn.filter=function(f){var r=[];for(var i=0;i<this.length;i++)if(f(this[i]))r.push(this[i]);return r}
-	})(Array.prototype);
 
 	/* jQuery base. */
 	var $ = function(selector, context) {
